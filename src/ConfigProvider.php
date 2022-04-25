31427c8cc452008a64c00fd150cb13171dbcc681
cexll <evanxian9@gmail.com>
2022-04-25 11:26:36 +0800

    add ConfigProvider

---
+++ src/ConfigProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace YogCloud\HyperfSoar;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'aspects' => [
+            ],
+            'dependencies' => [
+            ],
+            'commands' => [
+            ],
+            'annotations' => [
+                'scan' => [
+                    'paths' => [
+                        __DIR__,
+                    ],
+                ],
+            ],
+            'publish' => [
+                [
+                    'id' => 'config',
+                    'description' => 'The config for soar.',
+                    'source' => __DIR__ . '/../publish/soar.php',
+                    'destination' => BASE_PATH . '/config/autoload/soar.php',
+                ],
+            ],
+        ];
+    }
+}
